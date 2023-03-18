5e4d58fe9202422e48fe404a5deb4ec174b512b5
SatelliteHub <mael.account@icloud.com>
2023-03-18 12:02:52 +0000

    Update acj.js
    Comparative Judgment
    Implemented Maximum Likelihood Estimation(MLE)

--- acj.js
+++ acj.js
@@ -1,134 +1,339 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <base target="_top">
+</head>
+
+<body>
+  <!-- FOR DEBUGGING -->
+  <div id="result"></div>
+
+
+  <h1>Adaptive Comparison Judgment<br> (RELEASE CANDIDATE. ONLY 4 QUESTIONS)</h1><br/>
+  <p id="instruction">Loading…<br></p><br/>
+
+  <!-- Color Table -->
+  <form id="myForm" onsubmit="handleFormSubmit(this)">
+    <table id="colorTable">
+      <tr id="colorExplanationRow">
+        <th>Color A</th>
+        <th>Color B</th>
+      </tr>
+      <tr id="colorRow">
+        <td id="color_A_Cell"> </td>
+        <td id="color_B_Cell"> </td>
+      </tr>
+    </table><br>
+
+    <!-- Current color value shown -->
+    <div class="form-group">
+      <input type="hidden" id="colorShown_A" name="colorShown_A" value="150" >
+      <input type="hidden" id="colorShown_B" name="colorShown_B" value="150" >
+    </div>
+
+    <!-- User input space -->
+    <div class="form-group">
+      <input type="radio" id="radio_A" name="userAnswer" value="A" disabled> <label>A</label> <br><br>
+      <input type="radio" id="radio_B" name="userAnswer" value="B" disabled> <label>B</label> <br><br>
+      <button type="submit" id="submit" class="button" disa>Submit</button>
+      <p id="counterDisplay">Questions Answered: 0 out of 45</p>
+    </div>
+  </form>
+</body>
+
+
+
+
+
+
+
+
+
+
+
 <script>
-  var i = 0;
-  var counter;
-
-  var numbers_A = [0, 25, 50, 75, 100, 125, 175, 200, 225, 250]; //150 omitted because default value
-  var numbers_B = [];
-  var shuffledNumbers_A = shuffle(numbers_A);
-
-  var userChoice;
-
-  function changeColor() {
-    if (i < shuffledNumbers_A.length) {
-      const inputCompare = {value: shuffledNumbers_A[i]};
-      const index = findInsertionIndex(numbers_B, inputCompare);
-      numbers_B.splice(index, 0, inputCompare);
-
-      var colorNew_A = "rgb(" + shuffledNumbers_A[i] + ", " + shuffledNumbers_A[i] + ", " + shuffledNumbers_A[i] + ")";
-      var colorNew_B = "rgb(" + numbers_B[i] + ", " + numbers_B[i] + ", " + numbers_B[i] + ")";
-      document.getElementById("color_A_Cell").style.background = colorNew_A;
-      document.getElementById("color_B_Cell").style.background = colorNew_B;
-      counter = i + 2;
-      counterDisplay.innerHTML = "Question " + counter + "/11";
-      i++;
-    }
-    else {
-      const result = numbers_B.map((x) => x.value); // RESULT IS AN ARRAY TO STORE NEW ORDER
-      document.getElementById("sorting-result").textContent = result; // DISPLAY THE RESULT ORDERED ARRAY TO THE USER
-
-      finishedMessage.innerHTML = "You have Completed the ACJ Assessment. You will be now redirected to the <b>CTJ Assessment</b>.";
-      counterDisplay.innerHTML = "Question XX/11";
-      radio_A.disabled = true;
-      radio_B.disabled = true;
-      submit.disabled = true;
-    }
-  }
-
-  function findInsertionIndex(arr, item) { // arr IS NUMBER_B and item IS input_Compare
-    let low = 0;
-    let high = arr.length; // EQUIVALENT TO number_B.length
-
-    while (low < high) {
-      const mid = Math.floor((low + high) / 2);
-      if (userChoice == 'A') {
-        high = mid;
-      } else {
-        low = mid + 1;
-      }
-    }
-    return low;
-  }
-
-
-  function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array;
-  }
-
-
-  // PREVENT THE REDIRECTION AFTER CLICKING SUBMIT
-  function preventFormSubmit(){
-    var forms=document.querySelectorAll('form');
-    for (var i = 0; i < forms.length; i++){
-      forms[i].addEventListener('submit',function(event){
+  // Redirect prevention when user clicks "submit"
+  function preventFormSubmit() {
+    var forms = document.querySelectorAll('form');
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].addEventListener('submit', function(event) {
         event.preventDefault();
       });
     }
+  } window.addEventListener('load', preventFormSubmit);
+  
+  let colorValues = []; // Initialize an empty array to store the data
+  var i = 0;
+  
+  function getColorValuesFromServer(){
+    google.script.run.withSuccessHandler(data => {
+      // Store the data in the array
+      colorValues = data;
+      // Call the new function to assign two values from the array to a variable
+      changeColorValues();
+    }).withFailureHandler(er => {
+      // Set the innerHTML property of the 'result' element to display the error message
+      document.getElementById("result").innerHTML = "Error Occured";
+    }).getColorValues()
   }
-  window.addEventListener('load',preventFormSubmit);
 
+  function changeColorValues() {
+    // Randomize array's row between 0 and 10 without having the same values for a and b
+    do {
+      var a = Math.floor(Math.random() * 10);
+      var b = Math.floor(Math.random() * 10);
+    } while (a === b);
+    // Assign the first two values from the 'colorValues' array to a variable
+    let color1 = colorValues[a];
+    let color2 = colorValues[b];
+    // DEBUGGING
+    // document.getElementById("result").innerHTML = `Color 1: ${color1}<br>Color 2: ${color2}`;
+    
+    // Show instruction (Initial message was "Loading…")
+    document.getElementById("instruction").innerHTML = "Please select the <b>Lighter Shade</b>.";
+    submit.disabled = false;
+    radio_A.disabled = false;
+    radio_B.disabled = false;
+    // Color for A and B
+    var colorNew_A = "rgb(" + color1 + ", " + color1 + ", " + color1 + ")";
+    var colorNew_B = "rgb(" + color2 + ", " + color2 + ", " + color2 + ")";
+    document.getElementById("color_A_Cell").style.background = colorNew_A;
+    document.getElementById("color_B_Cell").style.background = colorNew_B;
+    // Get current color data for Google Sheet
+    document.getElementById("colorShown_A").value = color1;
+    document.getElementById("colorShown_B").value = color2; 
+    //-------
+  }
 
-  //FUNCTION WHEN USER CLICKS SUBMIT BUTTON
+  // Call the function when the page loads
+  window.onload = getColorValuesFromServer;
+
+  var counter = 1;
   function handleFormSubmit(formObject){
-    google.script.run.processForm(formObject);
-    document.getElementById("myForm").reset(); //Reset the radio button selection made by the user
-    document.getElementById("colorShown_A").value = shuffledNumbers_A[i];
-    document.getElementById("colorShown_B").value = numbers_B[i];
-    userChoice = document.getElementByName("userAnswer");
-    changeColor();
-  }
-
-
-//--------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------
-
-
-<!-- MAXIMUM LIELIHOOD ESTIMATION -->
-
-// Define the log-likelihood function for a normal distribution
-function logLikelihood(params, data) {
-  const [mu, sigma] = params;
-  const N = data.length;
-  const sum = data.reduce((acc, x) => acc + (x - mu) ** 2, 0);
-  const ll = -N / 2 * Math.log(2 * Math.PI * sigma ** 2) - sum / (2 * sigma ** 2);
-  return ll;
-}
-
-// Define the maximum likelihood estimation function
-function maximumLikelihood(data, logLikelihood, initialParams, tolerance, maxIterations) {
-  let bestParams = initialParams;
-  let bestLL = logLikelihood(bestParams, data);
-  let iteration = 0;
-  let delta = Infinity;
-  
-  while (iteration < maxIterations && delta > tolerance) {
-    // Generate a new set of parameters and evaluate the log-likelihood
-    const newParams = bestParams.map(p => p + (Math.random() - 0.5));
-    const newLL = logLikelihood(newParams, data);
-    
-    // Update the best parameters if the log-likelihood is higher
-    if (newLL > bestLL) {
-      bestParams = newParams;
-      bestLL = newLL;
-      delta = Math.abs(newLL - bestLL);
+    if (document.getElementById('radio_A').checked || document.getElementById('radio_B').checked) {
+      if (i < 3) { //44
+        google.script.run.processForm(formObject);
+        // Reset the radio button selection made by the user
+        document.getElementById("myForm").reset();
+        // Call the changeColorValues function
+        changeColorValues();
+        counter = i + 1;
+        counterDisplay.innerHTML = "Questions Answered: " + counter + " out of 45";
+        i++;
+      }
+      else {
+        google.script.run.processForm(formObject);
+        getUserSelectedColorValuesFromServer().then(userSelectedColorValues => {
+          var array = userSelectedColorValues;
+          var subArray = [array];
+          var userSelectedData = rankOrderAll(subArray);
+          google.script.run.withSuccessHandler(() => {
+            // Do something after showRankOrder has completed
+          }).showRankOrder(userSelectedData);
+        });
+        counterDisplay.innerHTML = "You will be now redirected to the <b>CTJ Assessment</b>.";
+        submit.disabled = true;
+        setTimeout(function() {
+          window.open("https://script.google.com/macros/s/AKfycbx_gws5D3fnU4Mhg-OkSiyNICEmZqyhDP04W_7TZHE0WlBYSr6OQCHD887hc9M3NxjKqw/exec");
+        }, 3000); // 3 seconds
+      }
     }
-    
-    iteration++;
+    else {
+      alert("Please select an answer.");
+    }
   }
   
-  return bestParams;
-}
+  function getUserSelectedColorValuesFromServer() {
+    return new Promise((resolve, reject) => {
+      google.script.run.withSuccessHandler(data => {
+        // Resolve the promise with the data
+        resolve(data);
+      }).getUserColorValues();
+    });
+  }
 
+
+  // MAXIMUM LIKELIHOOD ESTIMATION //
+  // Calculate the likelihood of observing the data given the probability of a specific value in the user's choice
+  function likelihood(data, p) {
+    var likelihood = 1;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] === p) {
+        likelihood *= 1 / 256;
+      }
+      else {
+        likelihood *= 255 / 256;
+      }
+    }
+    return likelihood;
+  }
+
+  // Function to find the MLE of the probability of all values on user's choice for each data set
+  function mleMultipleData(dataSets) {
+    var mleResults = [];
+    for (var i = 0; i < dataSets.length; i++) {
+      var data = dataSets[i];
+      var mleValues = [];
+      for (var p = 1; p <= 255; p++) {
+        var likelihood_p = likelihood(data, p);
+        mleValues.push({ value: p, likelihood: likelihood_p });
+      }
+      mleValues.sort(function (a, b) { return b.likelihood - a.likelihood; });
+      mleResults.push(mleValues);
+    }
+    return mleResults;
+  }
+
+  // Function to rank the order of the MLE for all data sets
+  function rankOrderAll(data) {
+    // Calculate the likelihoods for all values of user's choice across all data sets
+    const allLikelihoods = mleMultipleData(data);
+
+    // Combine the likelihoods for each value of user's choice across all data sets
+    const combinedLikelihoods = {};
+    for (const likelihoods of allLikelihoods) {
+      for (const { value, likelihood } of likelihoods) {
+        if (combinedLikelihoods[value]) {
+          combinedLikelihoods[value] *= likelihood;
+        } 
+        else {
+          combinedLikelihoods[value] = likelihood;
+        }
+      }
+    }
+
+    // Sort the values of user's choice by their combined likelihoods, in descending order
+    const sortedValues = Object.keys(combinedLikelihoods).sort((a, b) => combinedLikelihoods[b] - combinedLikelihoods[a]);
+
+    // Create a Set of all values present in the data array, including "0"
+    const dataSetValues = new Set(data.flat().concat([0]));
+
+    // Filter the sortedValues array to only include values that are present in the dataSetValues Set
+    const filteredValues = sortedValues.filter(value => dataSetValues.has(parseInt(value)));
+
+    // Return the filtered values as an array of integers
+    return filteredValues.map(value => parseInt(value));
+  }
+
+
+  function rankOrderAll(data) {
+    // Calculate the likelihoods for all values of the dice across all data sets
+    const allLikelihoods = mleMultipleData(data);
+
+    // Combine the likelihoods for each value of the dice across all data sets
+    const combinedLikelihoods = {};
+    for (const likelihoods of allLikelihoods) {
+      for (const { value, likelihood } of likelihoods) {
+        if (combinedLikelihoods[value]) {
+          combinedLikelihoods[value] *= likelihood;
+        } 
+        else {
+          combinedLikelihoods[value] = likelihood;
+        }
+      }
+    }
+
+    // Sort the values of the dice by their combined likelihoods, in descending order
+    const sortedValues = Object.keys(combinedLikelihoods).sort((a, b) => combinedLikelihoods[b] - combinedLikelihoods[a]);
+
+    // Create a Set of all values present in the data array, including "0"
+    const dataSetValues = new Set(data.flat().concat([0]));
+
+    // Filter the sortedValues array to only include values that are present in the dataSetValues Set
+    const filteredValues = sortedValues.filter(value => dataSetValues.has(parseInt(value)));
+
+    // Return the filtered values as an array of integers
+    return filteredValues.map(value => parseInt(value));
+  }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+  * {
+    font-family: Arial;
+    text-align: center;
+  }
+
+  table {
+    width: 70%;
+    border-collapse: collapse;
+    margin: 0 auto;
+  }
+
+  #color_A_Cell {
+    width: 100px;
+    height: 100px;
+    background-color: rgb(0, 0, 0);
+    margin: 10px;
+    border: 1px solid black;
+  }
+
+  #color_B_Cell {
+    width: 100px;
+    height: 100px;
+    background-color: rgb(0, 0, 0);
+    margin: 10px;
+    border: 1px solid black;
+  }
+
+  .button {
+    display: inline-block;
+    background-color: #0569b9;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s ease;
+  }
+
+  .button:hover {
+    background-color: #448fcb;
+  }
+
+  .button:active {
+    background-color: #0569b9;
+    transform: translateY(1px);
+  }
+
+  button:disabled {
+    background-color: gray;
+    color: white;
+    cursor: not-allowed;
+    pointer-events: none;
+  }
+
+  //DARKMODE
+  body {
+    background-color: white;
+    color: black;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    body {
+      background-color: black;
+      color: #d6d6d6;
+    }
+</style>
+
+</html>
